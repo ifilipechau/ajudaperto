3397cf172eca79221d2a0ad3c424115047df2bee
ifilipechau <filipechau.dev@gmail.com>
2025-10-15 12:03:54 +0200

    Add initial implementation of Database class and create empty controller, model, and view files

---
+++ app/config/Database.php
@@ -0,0 +1,17 @@
+<?php 
+// Configuracao da conexao com a Base de Dados
+Class Database {
+    private $host = "localhost";
+    private $db_name = "ajudaperto";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    
+
+
+
+
+}
+
+?>
