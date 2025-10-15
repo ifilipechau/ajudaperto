6b07092cb29b5f831e63a2ddcc7cc373e8a221f3
ifilipechau <filipechau.dev@gmail.com>
2025-10-15 12:08:47 +0200

    Refactor Database class to improve connection method implementation

--- app/config/Database.php
+++ app/config/Database.php
@@ -7,10 +7,20 @@ Class Database {
     private $password = "";
     public $conn;
 
-    
-
-
-
+    public function connect() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO(
+                "mysql:host" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+            $this->conn->exec("set names utf8mb4");
+        } catch (PDOException $e) {
+            echo "Erro de conexão: " . $e->getMessage();
+        }
+        return $this->conn;
+    }
 
 }
 
