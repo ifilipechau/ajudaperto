2a6f20e88b94d6494f7a81d5e35efa7a07522e66
ifilipechau <filipechau.dev@gmail.com>
2025-10-18 09:00:57 +0200

    Update database connection settings to use 'solidariedade_mvp' and improve error handling

--- app/config/Database.php
+++ app/config/Database.php
@@ -2,22 +2,23 @@
 // Configuracao da conexao com a Base de Dados
 Class Database {
     private $host = "localhost";
-    private $db_name = "ajudaperto";
+    private $db_name = "solidariedade_mvp";
     private $username = "root";
     private $password = "";
     public $conn;
 
-    public function connect() {
+    public function getConnection() {
         $this->conn = null;
+
         try {
             $this->conn = new PDO(
-                "mysql:host" . $this->host . ";dbname=" . $this->db_name,
+                "mysql:host={$this->host};dbname={$this->db_name};charset=utf8mb4",
                 $this->username,
                 $this->password
             );
-            $this->conn->exec("set names utf8mb4");
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
-            echo "Erro de conexão: " . $e->getMessage();
+            echo "Falha na conexão: " . $e->getMessage();
         }
         return $this->conn;
     }
