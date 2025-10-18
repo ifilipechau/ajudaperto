f1d5dff5aeaed9d8186f054d8117de81b2a703e9
ifilipechau <filipechau.dev@gmail.com>
2025-10-18 09:31:58 +0200

    implement InstituicaoController with cadastrar method for handling instituion registration

---
+++ app/controllers/InstituicaoController.php
@@ -0,0 +1,36 @@
+<?php 
+// Controlador responsável por receber dados do formulário e acionar o modelo
+
+require_once __DIR__ . "/../config/Database.php";
+require_once __DIR__ . "/../models/Instituicao.php";
+
+class InstituicaoController {
+
+    public function cadastrar() {
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $database = new Database();
+            $db = $database->getConnection();
+
+            $instituicao = new Instituicao($db);
+            $instituicao->nome = $_POST['nome'];
+            $instituicao->descricao = $_POST['descricao'];
+            $instituicao->cidade = $_POST['cidade'];
+            $instituicao->bairro = $_POST['bairro'];
+            $instituicao->endereco = $_POST['endereco'];
+            $instituicao->telefone = $_POST['telefone'];
+            $instituicao->email = $_POST['email'];
+            $instituicao->website = $_POST['website'];
+
+            if ($instituicao->criar()) {
+                header("Location: /views/instituicao_cadastro.php?sucesso=1");
+                exit;
+            } else {
+                header("Location: /views/instituicao_cadastro.php?erro=1");
+                exit;
+            }
+        }
+    }
+}
+
+
+?>
